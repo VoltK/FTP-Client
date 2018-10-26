addfb8484945f96fb41af608bd0ce07596fc3efa
Khud <khud44@icloud.com>
2018-10-26 15:49:40 -0500

    Changed the way to read command line

--- ftp_client.py
+++ ftp_client.py
@@ -1,4 +1,4 @@
-from ftplib import FTP, error_perm, error_temp
+from ftplib import FTP, all_errors
 import socket, os
 
 
@@ -10,67 +10,75 @@ def help_msg():
     print('\t"cwd dirname" to change working directory')
     print('\t"mkd dirname" to create directory')
     print('\t"rmd dirname" to delete directory')
+    print('\t"rename filename newfilename" to rename file')
     print('\t"exit" to exit from program')
 
 
-def check_commands(command_, func, *args):
-    if len(command_.split()) > 1:
+def check_commands(func, *args):
         try:
             func(*args)
-        except:
-            print("Command failed")
-    else:
-        print('blank argument, try again with "your_command your_argument. Enter \"help\" for more details"')
+        except Exception as e:
+            print(f"Command failed: {e}")
 
 
-def get_file(ftp_, command_):
-        filename = command_.split()[1]
+def get_file(ftp_, filename):
         ftp_.retrbinary("RETR " + filename, open(filename, 'wb').write)
         print(f"{filename} successfully downloaded")
 
 
-def upload_file(ftp_, command_):
-        file_path = command_.split()[1]
+def upload_file(ftp_, file_path):
         filename = os.path.basename(file_path)
         with open(file_path, 'rb') as f:
             ftp_.storbinary("STOR " + filename, f)
             print(f"{filename} successfully uploaded")
 
 
-def delete_file(ftp_, command_):
-        ftp_.delete(command_.split()[1])
-        print(f'{command_.split()[1]} successfully deleted')
+def delete_file(ftp_, filename):
+        ftp_.delete(filename)
+        print(f'{filename} successfully deleted')
 
 
-def make_dir(ftp_, command_):
-        ftp_.mkd(command_.split()[1])
-        print(f"{command_.split()[1]} directory created")
+def make_dir(ftp_, dir_name):
+        ftp_.mkd(dir_name)
+        print(f"{dir_name} directory created")
 
 
-def delete_dir(ftp_, command_):
-        ftp_.rmd(command_.split()[1])
-        print(f"{command_.split()[1]} directory deleted")
+def rename_file(ftp_, file, renamed):
+    ftp_.rename(file, renamed)
+    print(f"{file} file was renamed to {renamed}")
 
 
-def change_wd(ftp_, command_):
-        ftp_.cwd(command_.split()[1])
-        print(f"working directory changed to {command_.split()[1]}")
+def delete_dir(ftp_, dir_name):
+        ftp_.rmd(dir_name)
+        print(f"{dir_name} directory deleted")
+
+
+def change_wd(ftp_, dir_name):
+        ftp_.cwd(dir_name)
+        print(f"working directory changed to {dir_name}")
 
 
 def main():
     hostname = input("> Enter your hostname: ")
     try:
-        with FTP(hostname) as ftp:
+        with FTP(hostname.strip()) as ftp:
 
             username = input("> Enter your username: ")
             password = input("> Enter your password: ")
 
             try:
-                ftp.login(username, password)
+                ftp.login(username.strip(), password.strip())
                 command = ""
                 while command != 'exit':
                     command = input("\n\tEnter command or \"help\"\n" + "> ")
 
+                    # parse command line
+                    if len(command.split()) == 3:
+                        command, argument1, argument2 = command.split()
+                    elif len(command.split()) == 2:
+                        command, argument1 = command.split()
+
+                    # check command and run functions
                     if command == "dir":
                         print("Directory content: ")
                         ftp.dir()
@@ -78,37 +86,37 @@ def main():
                     elif command == 'pwd':
                         print(ftp.pwd())
 
-                    elif command.split()[0] == "download":
-                        check_commands(command, get_file, ftp, command)
+                    elif command == "download":
+                        check_commands(get_file, ftp, argument1)
 
-                    elif command.split()[0] == "upload":
-                        check_commands(command, upload_file, ftp, command)
+                    elif command == "upload":
+                        check_commands(upload_file, ftp, argument1)
 
-                    elif command.split()[0] == "delete":
-                        check_commands(command, delete_file, ftp, command)
+                    elif command == "delete":
+                        check_commands(delete_file, ftp, argument1)
 
-                    elif command.split()[0] == "cwd":
-                        check_commands(command, change_wd, ftp, command)
+                    elif command == "cwd":
+                        check_commands(change_wd, ftp, argument1)
 
-                    elif command.split()[0] == 'mkd':
-                        check_commands(command, make_dir, ftp, command)
+                    elif command == 'mkd':
+                        check_commands(make_dir, ftp, argument1)
 
-                    elif command.split()[0] == 'rmd':
-                        check_commands(command, delete_dir, ftp, command)
+                    elif command == 'rmd':
+                        check_commands(delete_dir, ftp, argument1)
+
+                    elif command == 'rename':
+                        check_commands(rename_file, ftp, argument1, argument2)
 
                     elif command == 'help':
                         help_msg()
             except KeyboardInterrupt:
                 print('Ctrl+C was pressed. Exiting...')
 
-            except error_perm:
-                print("530 Login authentication failed. Exiting...")
-
-            except error_temp:
-                print("421 Idle timeout (30 seconds): closing control connection. Exiting...")
+            except all_errors as e:
+                print(f"FTP Error: {e}")
 
             except:
-                print('Error. Exiting...')
+                print(f'Error. Exiting...')
 
     except socket.gaierror:
         print("Failed to get address. Exiting...")
